b4ba4c44632c129b1d2380556dc887d54b1494d6
ilgiz98 <ilgizkasymov@gmail.com>
2016-12-25 13:07:21 +0600

    Создал проект, создал приложение authenticated, создал модель User

---
+++ assistance/authentication/models.py
@@ -0,0 +1,81 @@
+# coding=utf-8
+from __future__ import unicode_literals
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin, _user_has_perm, \
+    _user_has_module_perms
+from django.utils.translation import ugettext_lazy as _
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        """
+        Creates and saves a User with the given email, username and password.
+        """
+        if not email:
+            raise ValueError('User must have an email address')
+        user = self.model(email=self.normalize_email(email))
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """
+        Creates and saves a superuser with the given email and password.
+        """
+        user = self.create_user(email, password=password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(verbose_name=_('Имя'), max_length=30, blank=True)
+    last_name = models.CharField(verbose_name=_('Фамилия'), max_length=30, blank=True)
+    email = models.EmailField(verbose_name=_('Эл. адрес'), unique=True)
+    date_joined = models.DateTimeField(_('Дата регистрации'), auto_now_add=True)
+    is_active = models.BooleanField(verbose_name=_('Активен'), default=True)
+    is_staff = models.BooleanField(verbose_name=_('Администратор'), default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def get_full_name(self):
+        return self.first_name + ' ' + self.last_name
+
+    def get_short_name(self):
+        return self.first_name
+
+    def __unicode__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        # Active superusers have all permissions.
+        if self.is_active and self.is_superuser:
+            return True
+
+        # Otherwise we need to check the backends.
+        return _user_has_perm(self, perm, obj)
+
+    def has_perms(self, perm_list, obj=None):
+        for perm in perm_list:
+            if not self.has_perm(perm, obj):
+                return False
+        return True
+
+    def has_module_perms(self, app_label):
+        if self.is_active and self.is_superuser:
+            return True
+
+        return _user_has_module_perms(self, app_label)
+
+    @property
+    def is_admin(self):
+        return self.is_staff
+
+    class Meta:
+        verbose_name = _('Пользователь')
+        verbose_name_plural = _('Пользователи')
---
+++ assistance/main/settings.py
@@ -0,0 +1,88 @@
+# coding: utf-8
+import os
+import environ
+root = environ.Path(__file__) - 2
+env = environ.Env(DEBUG=(bool, False),)
+environ.Env.read_env()
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SITE_ROOT = root()
+DEBUG = env('DEBUG')
+DATABASES = {
+    'default': env.db()
+}
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+SECRET_KEY = env('SECRET_KEY')
+LANGUAGE_CODE = 'ru-Ru'
+TIME_ZONE = 'Asia/Bishkek'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+LETTUCE_SERVER_PORT = 4000
+ADMINS = (
+  ('admin', 'ilgizkasymov@gmail.com'),
+)
+
+AUTH_USER_MODEL = 'authentication.User'
+
+
+DJANGO_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+LOCAL_APPS = [
+    'authentication'
+]
+INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS
+MIDDLEWARE_CLASSES = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+ROOT_URLCONF = 'main.urls'
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+WSGI_APPLICATION = 'main.wsgi.application'
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
