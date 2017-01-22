c95201817078120a004e459c712d61dbab244d55
ilgiz <ilgizkasymov@gmail.com>
2017-01-22 22:57:17 +0600

    Создал приложение frontend создал HomeView для отображение главной страницы

--- assistance/main/urls.py
+++ assistance/main/urls.py
@@ -1,18 +1,3 @@
-"""assistance URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
@@ -23,5 +8,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'signin/$', views.SignInView.as_view(), name='sign_in'),
     url(r'signup/$', views.SignUpView.as_view(), name='sign_up'),
+    url(r'^', include('frontend.urls'), name='frontend')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
